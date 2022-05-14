a641e4ba5865a419ebfde822f016389f4a39428b
JustinAllenGirard <JustinAllenGirard@gmail.com>
2022-05-14 16:29:09 -0500

    Adding routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import SomeRoute from "./routes/someRoute";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="someRoute" element={<SomeRoute />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/routes/someRoute.js
@@ -0,0 +1,7 @@
+export default function SomeRoute() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Some Route</h2>
+    </main>
+  );
+}
